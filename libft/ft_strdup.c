516948d9671aa64190d5c1de8239e3fd519ce6f9
shenghongzhong <howdavidlearncoding@gmail.com>
2024-02-08 19:59:08 +0000

    Norminetted libft

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -23,9 +23,9 @@ char	*ft_strdup(const char *s1)
 	size_t	len;
 
 	len = ft_strlen(s1) + 1;
-	s2 = malloc(len *sizeof(char));
+	s2 = malloc(len * sizeof(char));
 	if (!s2)
-		return(NULL);
+		return (NULL);
 	ft_strlcpy(s2, s1, len);
 	return (s2);
 }
